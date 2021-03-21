bd787300aa26713182c2ef7fc5361c373bea3bb6
tsuremu <tsureta_hiromu@yahoo.co.jp>
2021-03-22 01:35:35 +0900

    動作確認完了

--- public/js/app.js
+++ public/js/app.js
@@ -1,75 +1,75 @@
 let auth0 = null;
 
-const fetchAuthConfig = () => fetch("/auth_config.json");
+window.onload = async () => {
+  await configureClient();
 
-const configureClient = async () => {
-    const response = await fetchAuthConfig();
-    const config = await response.json();
+  // NEW - update the UI state
+  updateUI();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
   
-    auth0 = await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId
-    });
-  };
-
-  window.onload = async () => {
-
-    // .. code ommited for brevity
-  
+    // Process the login state
+    await auth0.handleRedirectCallback();
+      
     updateUI();
   
-    const isAuthenticated = await auth0.isAuthenticated();
-  
-    if (isAuthenticated) {
-      // show the gated content
-      return;
-    }
-  
-    // NEW - check for the code and state parameters
-    const query = window.location.search;
-    if (query.includes("code=") && query.includes("state=")) {
-  
-      // Process the login state
-      await auth0.handleRedirectCallback();
-      
-      updateUI();
-  
-      // Use replaceState to redirect the user away and remove the querystring parameters
-      window.history.replaceState({}, document.title, "/");
-    }
-  };
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
 
-  const updateUI = async () => { 
-    const isAuthenticated = await auth0.isAuthenticated();
-  
-    document.getElementById("btn-logout").disabled = !isAuthenticated;
-    document.getElementById("btn-login").disabled = isAuthenticated;
-    
-    // NEW - add logic to show/hide gated content after authentication
-    if (isAuthenticated) {
-      document.getElementById("gated-content").classList.remove("hidden");
-  
-      document.getElementById(
-        "ipt-access-token"
-      ).innerHTML = await auth0.getTokenSilently();
-  
-      document.getElementById("ipt-user-profile").textContent = JSON.stringify(
-        await auth0.getUser()
-      );
-  
-    } else {
-      document.getElementById("gated-content").classList.add("hidden");
-    }
-  };
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
 
-  const login = async () => {
-    await auth0.loginWithRedirect({
-      redirect_uri: window.location.origin
-    });
-  };
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
 
-  const logout = () => {
-    auth0.logout({
-      returnTo: window.location.origin
-    });
-  };
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const updateUI = async () => { 
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+  
+  // NEW - add logic to show/hide gated content after authentication
+  if (isAuthenticated) {
+    document.getElementById("gated-content").classList.remove("hidden");
+
+    document.getElementById(
+      "ipt-access-token"
+    ).innerHTML = await auth0.getTokenSilently();
+
+    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+      await auth0.getUser()
+    );
+
+  } else {
+    document.getElementById("gated-content").classList.add("hidden");
+  }
+};
+
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
+
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+};
