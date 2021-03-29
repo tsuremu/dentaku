64f8635219bc4b41bc276efaeae5c5259454e738
Hosoda0111 <73287450+Hosoda0111@users.noreply.github.com>
2021-03-29 21:55:49 +0900

    Web_designからマージ

--- public/js/index.js
+++ public/js/index.js
@@ -20,7 +20,7 @@ var result = document.getElementById("result");
 function edit(elem) {
     result.value = result.value + elem.value;
 }
-
+/*
 $(function() {
     $('#button18').click(
       function() {
@@ -43,3 +43,44 @@ $(function() {
 			}
 		);
 	  });
+*/
+//async:非同期処理の構文
+//crossDomain：ドメインからデータ持ってくる
+//method：メソッド
+
+$(function () {
+
+	$('#button18').click(
+		function () {
+			const settings = {
+				"async": true,
+				"crossDomain": true,
+				"url": "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5",
+				"method": "GET",
+				"headers": {
+					"x-rapidapi-key": "94552e227cmsh346d3bfe1dd31bap177b7fjsn7b5c4a0d0bfe",
+					"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com"
+				}
+			};
+			$.ajax(settings).done(
+				function (res) {
+					//取得成功したら実行する処理
+					let result = res.data[0];
+					// app_temp: -61.2
+					// aqi: null
+
+					let obj = {
+						age: 12,
+						name: "tom",
+					}
+
+					console.log(obj.age);
+					console.log(obj.name);
+
+					console.log(result);
+					$('#result2').html(" solar_rad " + result.solar_rad + " vis " + result.vis);
+				}
+			);
+		}
+	);
+});
